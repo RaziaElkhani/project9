a5b4e1b8ce6a340c407856a2253f5eddfff1b6cc
elkha <elkha@10.0.0.142>
2025-01-30 12:33:28 -0800

    adding few variable

---
+++ src/demo2/TestDemo.java
@@ -0,0 +1,14 @@
+package demo2;
+
+public class TestDemo {
+public static void main(String[] args) {
+	String fullName="Zaid Elkhani";
+	int age =4;
+	String city="Dublin";
+	boolean isHeReadyForkg = true;
+	
+	System.out.println(fullName);
+	System.out.println(age);
+	System.out.println(city);
+}
+}
