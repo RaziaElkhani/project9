cb174f7bc779f0c7363ab52f0d5234f17119d9be
RaziaElkhani <elkhanirazia@gmail.com>
2025-01-30 13:40:28 -0800

    Update TestDemo.java

--- src/demo2/TestDemo.java
+++ src/demo2/TestDemo.java
@@ -3,9 +3,10 @@ package demo2;
 public class TestDemo {
 public static void main(String[] args) {
 	String fullName="Zaid Elkhani";
-	int age =4;
+	int age =18;
 	String city="Dublin";
-	boolean isHeReadyForkg = true;
+	String state ="CA";
+	boolean isHeReadyWorking = true;
 	
 	System.out.println(fullName);
 	System.out.println(age);
